5dda2f4fc95cd0480fdd8761db4942599b53d55e
salissalmann <i200489@nu.edu.pk>
2024-03-25 03:26:21 +0500

    fix: port change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('./index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9090)
+    app.run(host="0.0.0.0", port=5000, debug=True)
